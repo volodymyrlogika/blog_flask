3ab86ef9227041b518d4e67354ce439c6f972cc1
volodymyr <volodymyrlogika@gmail.com>
2025-11-22 14:47:14 +0200

    Додали категорії

--- app.py
+++ app.py
@@ -13,6 +13,15 @@ def get_all_articles():
     conn.close()
     return data
 
+def get_all_categories():
+    conn = sqlite3.connect('blog.db')
+    conn.row_factory = sqlite3.Row 
+    cursor = conn.cursor()
+    cursor.execute('''SELECT * FROM categories''')
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
 def search_articles(search):
     conn = sqlite3.connect('blog.db')
     conn.row_factory = sqlite3.Row 
@@ -36,18 +45,29 @@ def get_article(article_id):
     conn.close()
     return data
 
+def get_articles_by_category(category_id):
+    conn = sqlite3.connect('blog.db')
+    conn.row_factory = sqlite3.Row 
+    cursor = conn.cursor()
+    cursor.execute('''SELECT * FROM articles WHERE category_id=?''', [category_id])
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
 
 @app.route("/") # Вказуємо url-адресу для виклику функції
 def index():
     message = 'Привіт з пайтону!'
     articles = get_all_articles()
-    return render_template("index.html", articles=articles, message=message, name="Володимир") #Результат, що повертається у браузер
+    categories = get_all_categories()
+    return render_template("index.html", articles=articles, message=message, name="Володимир", categories=categories) #Результат, що повертається у браузер
 
 
 @app.route("/article/<int:article_id>")
 def article_page(article_id):
     article = get_article(article_id)
-    return render_template('article_page.html', article=article)
+    categories = get_all_categories()
+    return render_template('article_page.html', article=article, categories=categories)
 
 @app.route("/search")
 def search_page():
@@ -57,6 +77,12 @@ def search_page():
     
     articles = search_articles(search)
     return render_template('index.html', articles=articles)
+
+@app.route("/category/<int:category_id>")
+def category_page(category_id):
+    articles = get_articles_by_category(category_id)
+    categories = get_all_categories()
+    return render_template('index.html', articles=articles, categories=categories)
 
 
 if __name__ == "__main__":
