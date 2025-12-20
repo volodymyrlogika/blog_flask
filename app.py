e2665eb348182d778e9ee1847ac51741da34e8a7
volodymyr <volodymyrlogika@gmail.com>
2025-12-20 16:26:42 +0200

    Add comment functionality to article page and update database schema

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os 
 
 app = Flask(__name__)  # Створюємо веб–додаток Flask
 
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'fdvhifdvhvu13213')
 
 def get_all_articles():
     conn = sqlite3.connect('blog.db')
@@ -57,6 +59,25 @@ def get_category_articles(category_id):
     return data
 
 
+def get_comments(article_id):
+    conn = sqlite3.connect('blog.db')
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute('''SELECT * FROM comments WHERE article_id=?''', [article_id])
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+
+def add_comment(article_id, email, content):
+    conn = sqlite3.connect('blog.db')
+    cursor = conn.cursor()
+    cursor.execute('''INSERT INTO comments (article_id, email, content) 
+                      VALUES (?, ?, ?)''', (article_id, email, content))
+    conn.commit()
+    conn.close()
+
+
 @app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
     message = 'Привіт з пайтону!'
@@ -66,11 +87,20 @@ def index():
     return render_template("index.html",  articles=articles, categories=categories, message=message, name="Володимир")
 
 
-@app.route("/article/<int:article_id>")
+@app.route("/article/<int:article_id>", methods=['POST', 'GET'])
 def article_page(article_id):
+    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        content = request.form.get('content')
+
+        if email and content:
+            add_comment(article_id, email, content)
+
     article = get_article(article_id)
     categories = get_all_categories()
-    return render_template('article_page.html', article=article, categories=categories)
+    comments = get_comments(article_id)
+    return render_template('article_page.html', article=article, categories=categories, comments=comments)
 
 
 @app.route("/search")
